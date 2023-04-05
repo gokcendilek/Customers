6ab920eeda58e5ae8c7ff2e819c3259ccd1c0e6d
GÖKÇEN DİLEK ALAK <GKEN DLEK ALAK@DESKTOP-SDT511I>
2023-04-05 23:27:32 +0300

    Proje dosyası ekle.

---
+++ Customers/Pages/Clients/DeleteClient.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Customers.Pages.Clients
+{
+    public class DeleteClientModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
